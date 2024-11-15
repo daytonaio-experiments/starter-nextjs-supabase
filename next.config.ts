3971d3055f3fb91a2f8dceae2d6a277b77eee521
Anurag Kainth <94080341+anuragkainth@users.noreply.github.com>
2024-11-16 00:03:45 +0530

    fix: updated with env configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_API_KEY: process.env.SUPABASE_API_KEY,
+  },
 };
 
 export default nextConfig;
